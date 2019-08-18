362123792f3fcf7acf4a12b607f260c97d980794
david.stclair <david@davidstclair.co.uki>
2019-08-18 07:16:43 +0100

    Adding Exercise 1.7

---
+++ ch1/exercise-1.7/exercise-1.7.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"log"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+          log.Fatal(err)
+		}
+
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		
+	}
+}
