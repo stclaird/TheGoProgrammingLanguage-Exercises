61f93b1abcb7e1fdeb136e5f4b4413cd3dae228c
david.stclair <david@davidstclair.co.uki>
2019-08-28 07:30:48 +0200

    finishing chap 1

---
+++ ch1/exercise-1.12/exercise-1.12.go
@@ -0,0 +1,73 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+var palette = []color.Color{color.RGBA{0x66, 0xCC, 0x00, 0xFF}, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	param_keys := r.URL.Query()        // grab the query string (an array)
+	cycles := param_keys.Get("cycles") // find the parameter "cycles" and assign its value to cycles var
+
+	// fmt.Fprintf(w, "%s\n", cycles)
+	cycles_int, e := strconv.Atoi(cycles)
+	if e != nil {
+		fmt.Println(e)
+	}
+	// fmt.Fprintf(w, "Cycles_int: %d\n", cycles_int)
+
+	lissajous(w, cycles_int)
+}
+
+func lissajous(out io.Writer, cycles_int int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	cycles := float64(cycles_int) // number of complete x oscillator revolutions
+	freq := rand.Float64() * 3.0  // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
